ccbcc53f04dcef9ccf11360c97abb07bb1b6af91
DiegoLIbanez <diegoibanez336@gmail.com>
2023-03-04 14:25:59 -0500

    configuracion email

--- helpers/email.js
+++ helpers/email.js
@@ -4,11 +4,11 @@ export const emailRegistro = async (datos) => {
     const {email,nombre,token} = datos;
 
     const transport = nodemailer.createTransport({
-        host:process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        host:"smtp.gmail.com",
+        port: 587,
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
+          user: "diegoibanez336@gmail.com",
+          pass: "jqggzdupqsdjukki",
         }
       });
 
@@ -19,7 +19,7 @@ export const emailRegistro = async (datos) => {
         text: 'Comprueba tu cuenta en Uptask',
         html:`<P>Hola: ${nombre} Comprueba tu cuenta en Uptask</P>
         <p>Tu cuenta ya esta casi lista solo debes comprobarla en el siguiente enlace:</p>
-        <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
+        <a href="https://uptask-12312.netlify.app/confirmar/${token}">Comprobar Cuenta</a>
         <p>Si tu no confirmaste tu cuenta, puedes ignorar el mensaje</p>
         `
       })
@@ -34,8 +34,8 @@ export const emailOlvidePassword = async (datos) => {
     secure: false,
     port: 587,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "diegoibanez336@gmail.com",
+      pass: "jqggzdupqsdjukki",
     }
     });
 
@@ -46,7 +46,7 @@ export const emailOlvidePassword = async (datos) => {
       text: 'Restablece tu Contraseña',
       html:`<P>Hola: ${nombre} has solicitado reestablecer tu contraseña en Uptask</P>
       <p>Sigue el siguinete enlace para generar un nueva contraseña:</p>
-      <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Contraseña</a>
+      <a href="https://uptask-12312.netlify.app/olvide-password/${token}">Reestablecer Contraseña</a>
       <p>Si tu no solicitaste este email, puedes ignorar el mensaje</p>
       `
     })
